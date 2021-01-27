7834f0a4bd247f6bd9bfc798eca7885818657193
Necralma <lihuel.almada@gmail.com>
2021-01-27 12:21:48 -0300

    init

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity, Touchable } from 'react-native';
+import alvaro from './assets/Captura.png'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hola Alvaro</Text>
+      <Image source={{uri: 'https://picsum.photos/200/300'}} style={styles.image}/>
+      <Image source={alvaro} style={styles.image}/>
+      <Button
+        color="blue"
+        title="Go!"
+        onPress={()=> console.log("Hola!")}
+      />
+      <Text style={styles.title}>Puede generar un   </Text>
+      <TouchableOpacity
+      onPress={()=> console.log("Hola!")}
+      >
+        <Text style={styles.title}>reclamo</Text>
+      </TouchableOpacity>
+      <Text style={styles.title}>o puede ir a</Text>
+      <TouchableOpacity
+      onPress={()=> console.log("Hola!")}
+      >
+        <Text style={styles.title}>gestion</Text>
+      </TouchableOpacity>
+      <Text style={styles.title}>con los botones</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +34,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "darkslateblue",
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    fontSize: 30, color: '#fff'
+  },
+  image:{
+    width:200,height:200, borderRadius: 100
+  }
 });
